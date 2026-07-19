c97bd73c25ad0b5acb52b0af8fb159765d335fae
johntoby <johntoby007@gmail.com>
2026-07-19 09:57:40 +0100

    updated variables files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,26 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
-  default     = "borderless-cluster"
 }
 
 variable "node_instance_type" {
   description = "EC2 instance type for EKS worker nodes"
   type        = string
-  default     = "t3.medium"
 }
 
 variable "github_repo" {
   description = "GitHub repository in the format owner/repo-name"
   type        = string
-  default     = "johntoby/borderless-items-manager"
 }
 
 variable "github_branch" {
   description = "GitHub branch allowed to assume the IAM role"
   type        = string
-  default     = "monitoring"
 }
 
 variable "db_user" {
@@ -49,9 +44,4 @@ variable "smtp_password" {
 variable "tags" {
   description = "Tags to apply to all resources"
   type        = map(string)
-  default = {
-    Project     = "borderless-items-manager"
-    Environment = "production"
-    ManagedBy   = "terraform"
-  }
 }
