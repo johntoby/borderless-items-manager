677d277a124f8b7f2d70103f472ce586c349a589
johntoby <johntoby007@gmail.com>
2026-07-19 22:24:40 +0100

    updated help deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -201,6 +201,18 @@ module "eks" {
         }
       }
     }
+    admin_user = {
+      principal_arn = "arn:aws:iam::266735814791:user/jtbaba2"
+      type          = "STANDARD"
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
   }
 
   tags = var.tags
